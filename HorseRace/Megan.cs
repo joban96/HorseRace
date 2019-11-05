89fd9ac62aa128f443beceb01f8b0ddaec0a271f
Joban <Jobansandhu418@gmail.com>
2019-11-06 08:46:20 +1300

    Add project files.

---
+++ HorseRace/Megan.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace HorseRace
+{
+    public class Megan : Punter
+    {
+        public Megan(Bet bet, Label MaximumBet, int Cash, Label label) : base(bet, MaximumBet, Cash, label)
+        {
+        }
+
+        public override void setPunterName()
+        {
+            Name = "Megan";
+        }
+    }
+}
